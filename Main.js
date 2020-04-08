3aea3c5b376d585164d517b1cda92f6289a6d8d0
zephyr11221 <sdoan@stuy.edu>
2020-04-07 22:30:42 -0400

    doCurl() functioning properly, follows an arc

--- Main.js
+++ Main.js
@@ -6,40 +6,43 @@ function setup (){
 	background (0);
 	createCanvas (800, 800);
 
-	frameRate(12);
-	initOrb = new Orb(400, 400, 70, 0.0, null, oR, oG, oB); // create the base Orb with x and y coords, size, flexibility, children, and original r, g, and b
+	frameRate(24);
+	initOrb = new Orb(400.0, 400.0, 70, 0.0, null, oR, oG, oB); // create the base Orb with x and y coords, size, flexibility, children, and original r, g, and b
 	let ch = []; //array of children
 
 	//tentacle bases
-	ch[0] = new Orb(470, 400, 50, 0.05, null, oR, oG, oB, initOrb);
-	ch[1] = new Orb(400, 470, 50, 0.05, null, oR, oG, oB, initOrb);	
-	ch[2] = new Orb(330, 400, 50, 0.05, null, oR, oG, oB, initOrb);
-	ch[3] = new Orb(400, 330, 50, 0.05, null, oR, oG, oB, initOrb);
+	ch[0] = new Orb(470.0, 400.0, 50, 0.1, null, oR, oG, oB, 1, initOrb);
+	ch[1] = new Orb(400.0, 470.0, 50, 0.1, null, oR, oG, oB, -1, initOrb);	
+	ch[2] = new Orb(330.0, 400.0, 50, 0.1, null, oR, oG, oB, -1, initOrb);
+	ch[3] = new Orb(400.0, 330.0, 50, 0.1, null, oR, oG, oB, 1, initOrb);
+
 	
 	//2nd segments
-	ch[0].children = new Orb(530, 400, 45, 0.05, null, oR, oG, oB, ch[0]);
-	ch[1].children = new Orb(400, 530, 45, 0.05, null, oR, oG, oB, ch[1]);
-	ch[2].children = new Orb(270, 400, 45, 0.05, null, oR, oG, oB, ch[2]);
-	ch[3].children = new Orb(400, 270, 45, 0.05, null, oR, oG, oB, ch[3]);
+	ch[0].children = new Orb(530, 400, 45, 0.3, null, oR, oG, oB, 1, ch[0]);
+	ch[1].children = new Orb(400, 530, 45, 0.3, null, oR, oG, oB, -1, ch[1]);
+	ch[2].children = new Orb(270, 400, 45, 0.3, null, oR, oG, oB, -1, ch[2]);
+	ch[3].children = new Orb(400, 270, 45, 0.3, null, oR, oG, oB, 1, ch[3]);
+
 	
 	//3rd segments
-	ch[0].children.children = new Orb(585, 400, 40, 0.05, null, oR, oG, oB, ch[0].children);
-	ch[1].children.children = new Orb(400, 585, 40, 0.05, null, oR, oG, oB, ch[1].children);
-	ch[2].children.children = new Orb(215, 400, 40, 0.05, null, oR, oG, oB, ch[2].children);
-	ch[3].children.children = new Orb(400, 215, 40, 0.05, null, oR, oG, oB, ch[3].children);
-	
+	ch[0].children.children = new Orb(585, 400, 40, 0.5, null, oR, oG, oB, 1, ch[0].children);
+	ch[1].children.children = new Orb(400, 585, 40, 0.5, null, oR, oG, oB, -1, ch[1].children);
+	ch[2].children.children = new Orb(215, 400, 40, 0.5, null, oR, oG, oB, -1, ch[2].children);
+	ch[3].children.children = new Orb(400, 215, 40, 0.5, null, oR, oG, oB, 1, ch[3].children);
+
 	//4th segments
-	ch[0].children.children.children = new Orb(625, 400, 35, 0.05, null, oR, oG, oB, ch[0].children.children);
-	ch[1].children.children.children = new Orb(400, 625, 35, 0.05, null, oR, oG, oB, ch[1].children.children);
-	ch[2].children.children.children = new Orb(175, 400, 35, 0.05, null, oR, oG, oB, ch[2].children.children);
-	ch[3].children.children.children = new Orb(400, 175, 35, 0.05, null, oR, oG, oB, ch[3].children.children);
-	
+	ch[0].children.children.children = new Orb(625, 400, 35, 0.7, null, oR, oG, oB, 1, ch[0].children.children);
+	ch[1].children.children.children = new Orb(400, 625, 35, 0.7, null, oR, oG, oB, -1, ch[1].children.children);
+	ch[2].children.children.children = new Orb(175, 400, 35, 0.7, null, oR, oG, oB, -1, ch[2].children.children);
+	ch[3].children.children.children = new Orb(400, 175, 35, 0.7, null, oR, oG, oB, 1, ch[3].children.children);
+
+
 	//5th segments
-	ch[0].children.children.children.children = new Orb(660, 400, 25, 0.05, null, oR, oG, oB, ch[0].children.children.children);
-	ch[1].children.children.children.children = new Orb(400, 660, 25, 0.05, null, oR, oG, oB, ch[1].children.children.children);
-	ch[2].children.children.children.children = new Orb(140, 400, 25, 0.05, null, oR, oG, oB, ch[2].children.children.children);
-	ch[3].children.children.children.children = new Orb(400, 140, 25, 0.05, null, oR, oG, oB, ch[3].children.children.children);
-	
+	ch[0].children.children.children.children = new Orb(660, 400, 25, 0.9, null, oR, oG, oB, 1, ch[0].children.children.children);
+	ch[1].children.children.children.children = new Orb(400, 660, 25, 0.9, null, oR, oG, oB, -1, ch[1].children.children.children);
+	ch[2].children.children.children.children = new Orb(140, 400, 25, 0.9, null, oR, oG, oB, -1, ch[2].children.children.children);
+	ch[3].children.children.children.children = new Orb(400, 140, 25, 0.9, null, oR, oG, oB, 1, ch[3].children.children.children);
+
 	initOrb.children = ch; // link the children to the base
 }
 
@@ -51,7 +54,7 @@ function draw () {
 }
 
 class Orb{
-	constructor(x, y, iSize, fl, cList, ir, ig, ib, iPar = null){
+	constructor(x, y, iSize, fl, cList, ir, ig, ib, curDir, iPar = null){
 		this.base = createVector(x,y); //base location of the Orb, used to calculate distance between them
 		this.cx = x; //current x and y of the orb
 		this.cy = y;
@@ -69,7 +72,7 @@ class Orb{
 		this.gdir = -1;
 		this.bdir = 1;
 
-		this.cdir = 1 //curl direction
+		this.cdir = curDir //curl direction
 		this.curl = 0 //current amount of curl
 	}
 
@@ -104,7 +107,7 @@ class Orb{
 				this.gdir = 1;
 			}
 			this.g += this.gdir*5;
-			print(this.r +" " + this.g + " " + this.b + "\n");
+			//print(this.r +" " + this.g + " " + this.b + "\n");
 		}
 	}
 
@@ -114,22 +117,27 @@ class Orb{
 		var cDist = p5.Vector.sub(this.base, createVector(this.cx, this.cy)); // the distance from this orb to its original location
 		var move;
 		//check what direction this orb will move
-		print(bDist.x + " " + bDist.y);
-		if (this.curl > 150*this.flex){
-			this.curl = -150;
+		//print(bDist.x + " " + bDist.y);
+		if (this.curl > 500*this.flex){
+			this.curl = -500*this.flex;
 			this.cdir *= -1;
 		}
-		this.curl+=1;
+		this.curl+=10*this.flex;
 		if (bDist.x == 0){
-			move = createVector(this.par.cx+(this.cdir*1), this.par.cy); // move the orb horizontally
-			move.limit(this.cdir*bDist.mag()); //restrict the movement to an arc by limiting it to the distance between the base distance
+			move = createVector(this.cx+(this.cdir*10*this.flex), this.cy); // move the orb horizontally
+			move.sub(createVector(400, 400));
+			//print(this.base.y-400);
+			move.setMag(abs(this.base.y-400)); //restrict the movement to an arc by limiting it to the distance between the base distance
 		}
 		if (bDist.y == 0){
-			move = createVector(this.par.cx, this.par.cy+(this.cdir*1)); // move the orb vertically
-			move.limit(this.cdir*bDist.mag()); //restrict the movement to an arc by limiting it to the distance between the base distance
+			move = createVector(this.cx, this.cy+(this.cdir*10*this.flex)); // move the orb vertically
+			move.sub(createVector(400, 400));
+			//print(this.base.x-400);
+			move.setMag(abs(this.base.x-400)); //restrict the movement to an arc by limiting it to the distance between the base distance
 		}
-		this.cx+=move.x;
-		this.cy+=move.y;
+		print(move.x + " " + move.y);
+		this.cx=400+move.x;
+		this.cy=400+move.y;
 	}
 	display(){
 		fill(color(this.r, this.g, this.b));
